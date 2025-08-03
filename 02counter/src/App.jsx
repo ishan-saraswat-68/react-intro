f8e9e477b13757f73df5d1332e389cc126709a5a
ishan-saraswat-68 <ishansaraswat68@gmail.com>
2025-08-03 11:41:42 +0530

    "tailwindprops"

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
 
 
-  let [counter,setcounter]=useState(5) //[var,function] = usestate(default value)
+  let [counter,setcounter]=useState(5) //[vaiable,function] = usestate(default value) used to reflect anywhere changes
 
   //let counter =5;
   
---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,30 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import Card from './components/Card'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  // let myObj ={
+  //   username : "ishan",
+  //   age : 20
+  // }
+
+
+  // let newArr =[1,2,3,4,5,6,7,8]
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-5'>Tailwind Test  </h1>
+
+      <Card username="chaiaurcode" btnText="click-me"/>
+
+      <Card username="Hitesh" btnText="vist-me"/>
+
+    </>
+  )
+}
+
+export default App
